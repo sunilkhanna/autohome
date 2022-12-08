9437f6cb28e15fec3d98ee5127f0adb341f00240
sunilkhanna <er.sunilkhanna@gmail.com>
2022-12-08 15:46:43 +0530

    1. Added template for GEO based selection 2. Added support for MQTT

---
+++ src/apis.py
@@ -0,0 +1,168 @@
+import ptvsd
+from fastapi import FastAPI
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from fastapi import Form
+import paho.mqtt.client as mqtt
+import time
+import configparser
+import json
+
+
+ptvsd.enable_attach(address=("localhost",8098))
+#ptvsd.wait_for_attach()
+ptvsd.break_into_debugger()
+#MQTT broker
+#mqttBroker= "d48173f9aa524e8e90ec3bc0d1c5927d.s1.eu.hivemq.cloud" #"telebit.cloud"
+mqttBroker="broker.hivemq.com"
+mqttPort=1883 #38395
+
+#-------Cloud-1 Noida setup----------//
+noida_topic_pub="ntp"
+noida_topic_sub="nts"
+
+#-------Cloud-2 SVPI setup----------//
+svpi_topic_pub="stp"
+svpi_topic_sub="sts"
+
+global stats
+stats='11'
+
+
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
+router = APIRouter()
+app = FastAPI()
+
+# the callback function, it will be triggered when receiving messages
+def on_message(mqttClient, userdata, msg):    
+    #mqttClient.publish( "nts",msg)   
+    print(f"{msg.topic} {msg.payload}")
+    recvdMsg=str(msg.payload.decode("utf-8")).strip()
+    print(recvdMsg.strip())
+    if(recvdMsg.strip()=='boot'):
+        #read the status file and publish the messages
+        print('Client or MCU rebooted..')
+        regainStats()    
+
+
+def on_connect(mqttClient, userdata, flags, rc):
+    #print(f"Connected with result code {rc}")
+    # subscribe, which need to put into on_connect
+    # if reconnect after losing the connection with the broker, it will continue to subscribe to the raspberry/topic topic 
+    if rc==0:
+        mqttClient.connected_flag=True       
+        mqttClient.subscribe(noida_topic_pub)
+       # print('connected..')
+
+def on_disconnect(mqttClient, userdata, rc):
+    #print('client disconnected...')
+    mqttClient.connected_flag=False
+    
+
+mqtt.Client.connected_flag=False #create flag in class
+mqttClient = mqtt.Client("HomeAutomation")
+mqttClient.on_connect = on_connect
+mqttClient.on_disconnect=on_disconnect
+mqttClient.on_message = on_message
+mqttClient.subscribe(noida_topic_pub)
+mqttClient.loop_start()
+
+mqttClient.connect(mqttBroker,mqttPort)
+
+while not mqttClient.connected_flag:
+    print('wait in loop')
+    time.sleep(1)
+
+
+
+def updateStats(data):
+    '''Method use to update the status of controllable items '''
+    config = configparser.RawConfigParser()
+    config.read('./src/stats.properties')
+    spr=data.split('-')
+    print("updating status to property file")
+    print(data,"--",spr[0].lower())    
+    config.set('GNDASection', spr[0].lower(),data.upper())
+    with open('./src/stats.properties', 'w') as configfile:
+        config.write(configfile)
+        configfile.close()
+
+def getStatsJson():
+    ''' get the data from stats file '''
+    configx = configparser.RawConfigParser()
+    configx.read('./src/stats.properties')
+    configSectionx=configx['GNDASection']
+    dictx={}
+    for key in configSectionx:
+        dictx.update({key.upper():configSectionx.get(key)})
+    jsonDataStats=json.dumps(dictx)                 
+    return jsonDataStats
+    
+def regainStats():
+    ''' Let's back to the original state after reboot '''
+    print("regain status on MCU boot")
+    config = configparser.RawConfigParser()
+    config.read('./src/stats.properties')
+    configSection=config['GNDASection']
+    for key in configSection:
+        print(configSection.get(key))
+        mqttClient.publish(noida_topic_pub,configSection.get(key))    
+    
+    
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):    
+    return templates.TemplateResponse("login.html", {"request": request})
+
+#@app.post("/login/")
+#async def login(username: str = Form(), password: str = Form()):
+ #   return {"username": username}
+
+
+@app.get("/outside1/{id}")
+async def read_but(request: Request,id: str):    
+    buttonStatus=id.split("-")
+    print('*********Button pressed')
+    if mqttClient.connected_flag:
+        result=mqttClient.publish(noida_topic_pub,id) ##------publish message to broker------------    
+        result.wait_for_publish()
+        print(result.mid,result.is_published(),result.__next__(),result.next(),result.rc)    
+        status = result[0]
+        if status == 0:
+            print("Message Sent  to topic-- " +noida_topic_pub)
+            updateStats(id)#udpate the stats
+            if buttonStatus[1]=="ON":
+                return "OFF"
+            return "ON"  
+        print("Failed to send message to topic"+ noida_topic_pub)
+        return buttonStatus[1]
+
+
+@app.post("/home")
+def do_something(request: Request, uname: str = Form(...), psw: str = Form(...)):
+    print(uname)
+    print("psw "+psw)
+    if(uname.__eq__('sunil') &  psw.__eq__('khanna')): 
+        ledStatus = getStatsJson()
+        #{"LED1":"LED1-ON","LED2":"LED2-ON","LED3":"LED3-ON","LED4":"LED4-OFF","LED5":"LED5-ON"}
+        return templates.TemplateResponse("home.html",  {"request": request,"ledStatus":ledStatus})
+    else:
+        return "Invalid Username/Password"
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+
+
---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import time
+
+def publishMsg(client,msg,topic):
+    msg_count = 0    
+    time.sleep(1)     
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
